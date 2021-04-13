7e8b43ab371f37d39382da73deb85c5b16862b83
klementj <klementj@gmail.com>
2021-04-13 14:44:27 +0200

    Finish exercise 04

---
+++ EX04ReadLine/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace EX04ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Firstname");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Lastname");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine($"Velkommen {firstName} {lastName}");
+
+
+            // Udregn summen af to tal
+            Console.WriteLine("Skriv det første tal");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Skriv det andet tal");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int sum = firstNumber + secondNumber;
+
+            Console.WriteLine($"Summen af dine tal er: {sum}");
+            Console.WriteLine(" ");
+
+            // Ekstraopgave 4.4 Arealudregning
+            Console.WriteLine("Udregn areal af to tal");
+            Console.WriteLine("Hvor lang er side eet i meter?");
+            int firstSide = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hvor lang er side to i meter?");
+            int secondSide = int.Parse(Console.ReadLine());
+
+            int area = firstSide * secondSide;
+
+            Console.WriteLine(area);
+        }
+    }
+}
