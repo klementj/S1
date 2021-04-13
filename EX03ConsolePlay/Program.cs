0ec9a5e3660c8ac52e291a9909fad9dcffa3ed3a
klementj <klementj@gmail.com>
2021-04-13 14:26:42 +0200

    Finish exercise 03

---
+++ EX03ConsolePlay/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EX03ConsolePlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Background color
+            Console.BackgroundColor = ConsoleColor.Green;
+            // Text color
+
+            // Console.WindowHeight = 10;
+            // Console.WindowWidth = 10;
+
+            Console.Beep(4000, 1000);
+        }
+    }
+}
