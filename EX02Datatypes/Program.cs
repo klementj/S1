a5a54cc72ad07a5ff82dc67f03bcfaa5339dc3e0
klementj <klementj@gmail.com>
2021-04-13 13:46:43 +0200

    Add exercise 02

---
+++ EX02Datatypes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EX02Datatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Karsten";
+            string lastName = "Hansen";
+            int age = 35;
+
+            Console.WriteLine($"{firstName} {lastName} er {age}år gammel");
+
+            
+        }
+    }
+}
