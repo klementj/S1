c04869cb46f435874e5c0add02797df6e3313cd4
klementj <klementj@gmail.com>
2021-04-13 13:35:20 +0200

    Finish exercise 01

---
+++ EX01HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EX01HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string schoolName = "AspIT";
+
+            Console.WriteLine(schoolName);
+
+            schoolName = "Campus Vejle";
+            Console.WriteLine(schoolName);
+
+            string motto = "Fokuseret Talent";
+
+            Console.WriteLine(schoolName);
+            Console.WriteLine(" ");
+            Console.WriteLine(motto);
+
+            Console.WriteLine("--------------");
+
+            Console.WriteLine($"{schoolName} er en skole i danmark hvor mottoet er: {motto}");
+        }
+    }
+}
