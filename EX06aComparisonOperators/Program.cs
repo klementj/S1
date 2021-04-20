ce7009d5cb83f55d688f56927808c77b243a02c0
klementj <klementj@gmail.com>
2021-04-20 13:57:19 +0200

    Finish exercise 06a

---
+++ EX06aComparisonOperators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EX06aComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 4;
+
+            Console.WriteLine(a == b); //false
+            Console.WriteLine(a > b); //false
+            Console.WriteLine(a <= b); //true
+            Console.WriteLine(a - b > 0); //false
+            Console.WriteLine(a + b > 0); //true
+            Console.WriteLine(a > 1 - b ); //true
+
+        }
+    }
+}
