95e6ef0ef4e4749cdf74ff43431937f62dee0625
klementj <klementj@gmail.com>
2021-04-20 14:08:37 +0200

    Finish exercise 06b

---
+++ EX06bComparisonOperators/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EX06bComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Opgave 1
+            // View -> Other Windows -> C# Interactive
+            // Leg med de forskellige operatorer i det interactive vindue
+
+            // Opgave 2
+            bool i = 17 < 10;
+            Console.WriteLine($"17 < 20 = {i}");
+
+            bool ii = 20 < 20;
+            Console.WriteLine($"20 < 20 = {ii}");
+
+            bool iii = 20 <= 20;
+            Console.WriteLine($"20 <= 20 = {iii}");
+
+            bool iv = 50 != 10 * 5;
+            Console.WriteLine($"50 != 10 * 5 = {iv}");
+
+            bool v = 27 - 0.15 >= 29 - 1.15;
+            Console.WriteLine($"27 - 0.15 >= 29 - 1.15 = {v}");
+
+            bool vi = 5 * 3 == 15;
+            Console.WriteLine($"5 * 3 == 15 = {vi}");
+
+            bool vii = 5 / 5 == 1;
+            Console.WriteLine($"5 / 5 == 1 = {vii}");
+        }
+    }
+}
